641e3c2fbcdc37733e77a6a946cf6a907ecc0aec
OQASergey <sergey.qa.63@protonmail.com>
2024-01-06 18:58:27 +0400

    Обновлен файл тестов приветствуенной страницы. Добавлена проверка скролла страницы и создания скриншотов

--- tests/test_01_page_attention.py
+++ tests/test_01_page_attention.py
@@ -1,8 +1,18 @@
-from selene import browser, have, be
+from selene import browser, have, query
 import requests
+from selene.core.wait import Command
 
 response1 = requests.get('http://localhost/theveil/')
 status1 = response1.status_code
+link = 'C:/_test_screenshots/test_01_page_attention/'
+
+def scroll(x: int, y: int) -> Command:
+    return Command(
+        f'scroll page by x {x} y {y}',
+        lambda browser: browser.driver.execute_script(
+            f'window.scrollBy({x}, {y});'
+        )
+    )
 
 def test_start():
     browser.open("/")
@@ -21,6 +31,16 @@ def test_attention_text():
     print('')
     print('Наличие текста предупреждения')
     browser.element('[id="at2"]').should(have.text("Данный сайт является небольшим справочным ресурсом для личного использования по настольной ролевой игре the Veil (авторы: Фрейзер и Кайл Симмонс). Сайт ничего не пропогандирует и не распространяет. Публиковать ссылку на текущий домен, поддомены и страницы на домене или поддомене в любом другом ресурсе строго воспрещается."))
+
+def test_scroll_down_up():
+    print('')
+    print('Скролл вниз на 10 000 px')
+    browser.perform(scroll(0, 10000))
+    browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_1_down.png'))
+    print('Скролл вверх на 20 000 px')
+    browser.perform(scroll(0, -20000))
+    browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_2_up.png'))
+    print(f'Скриншоты сохранены: {link}')
 
 response2 = requests.get('http://localhost/theveil/theveil/')
 status2 = response2.status_code
