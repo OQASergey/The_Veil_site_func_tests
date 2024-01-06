505f671f224d53b659de59035c0bc0a9238141ff
OQASergey <sergey.qa.63@protonmail.com>
2024-01-07 02:15:34 +0400

    Обновлен файл тестов приветственной страницы. Обновлены сообщения (названия тестов). Добавлены проверки боковых скроллов

--- tests/test_01_page_attention.py
+++ tests/test_01_page_attention.py
@@ -1,10 +1,11 @@
 from selene import browser, have, query
 import requests
 from selene.core.wait import Command
+import time
 
 response1 = requests.get('http://localhost/theveil/')
 status1 = response1.status_code
-link = 'C:/_test_screenshots/test_01_page_attention/'
+link = 'C:/_test_screenshots/The_Veil_site_func_tests/test_01_page_attention/'
 
 def scroll(x: int, y: int) -> Command:
     return Command(
@@ -17,7 +18,7 @@ def scroll(x: int, y: int) -> Command:
 def test_start():
     browser.open("/")
     print('')
-    print('Загрузка страницы')
+    print('Приветственная страница:Загрузка страницы')
     if status1 is 200:
         print('Статус код: ', status1, ' OK')
     else:
@@ -25,27 +26,37 @@ def test_start():
         browser.element('[id="reload-button"]').click()
 def test_attention_title():
     print('')
-    print('Наличие предупреждения')
+    print('Приветственная страница:Наличие предупреждения')
     browser.element('[id="at1"]').should(have.text("ПРЕДУПРЕЖДЕНИЕ"))
 def test_attention_text():
     print('')
-    print('Наличие текста предупреждения')
+    print('Приветственная страница:Наличие текста предупреждения')
     browser.element('[id="at2"]').should(have.text("Данный сайт является небольшим справочным ресурсом для личного использования по настольной ролевой игре the Veil (авторы: Фрейзер и Кайл Симмонс). Сайт ничего не пропогандирует и не распространяет. Публиковать ссылку на текущий домен, поддомены и страницы на домене или поддомене в любом другом ресурсе строго воспрещается."))
 
 def test_scroll_down_up():
     print('')
     print('Скролл вниз на 10 000 px')
     browser.perform(scroll(0, 10000))
+    time.sleep(1)
     browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_1_down.png'))
     print('Скролл вверх на 20 000 px')
     browser.perform(scroll(0, -20000))
+    time.sleep(1)
     browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_2_up.png'))
+    print('Скролл вправа на 1 000 px')
+    browser.perform(scroll(1000, 0))
+    time.sleep(1)
+    browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_3_right.png'))
+    print('Скролл влево на 2 000 px')
+    browser.perform(scroll(-2000, 0))
+    time.sleep(1)
+    browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_4_left.png'))
 
 response2 = requests.get('http://localhost/theveil/theveil/')
 status2 = response2.status_code
 def test_open_site():
     print('')
-    print('Переход на основной сайт')
+    print('Приветственная страница:Переход основной сайт')
     browser.element('[class="opbut"]').click()
     if status2 is 200:
         print('Статус код: ', status2, ' OK')
