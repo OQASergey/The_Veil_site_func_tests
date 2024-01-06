26a2dddd1f9a0ed4ab1094d6959500add8bb1951
OQASergey <sergey.qa.63@protonmail.com>
2024-01-06 20:05:07 +0400

    Обновлен файл тестов index.html. Добавлены проверки скролла страницы. Добавлено сохранение скриншотов

--- tests/test_03_page_index.py
+++ tests/test_03_page_index.py
@@ -1,8 +1,19 @@
-from selene import browser, have, be
+from selene import browser, have, query, be
 import requests
+from selene.core.wait import Command
+import time
 
 response = requests.get('http://localhost/theveil/theveil/')
 status = response.status_code
+link = 'C:/_test_screenshots/test_03_page_index/'
+
+def scroll(x: int, y: int) -> Command:
+    return Command(
+        f'scroll page by x {x} y {y}',
+        lambda browser: browser.driver.execute_script(
+            f'window.scrollBy({x}, {y});'
+        )
+    )
 
 def test_start():
     browser.open("/theveil/")
@@ -29,6 +40,22 @@ def test_click_on_sectors():
     browser.element('[class="primary-btn class-btn"][id="glr1"]').click()
     browser.should(have.url_containing('/theveil/gallery.html'))
     browser.open('/theveil/')
+
+def test_scroll_down_up():
+    print('')
+    time.sleep(1.5)
+    print('Скролл вниз на 10 000 px')
+    browser.perform(scroll(0, 10000))
+    time.sleep(1.5)
+    browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_1_down.png'))
+    print('')
+    time.sleep(1.5)
+    print('Скролл вверх на 20 000 px')
+    browser.perform(scroll(0, -20000))
+    time.sleep(1.5)
+    browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_2_up.png'))
+    browser.perform(scroll(0, 10000))
+    time.sleep(1)
 
 def test_up_buton():
     print('')
