e6710f6cdae780c3e9b02d6d74f6d21c0e1e520c
OQASergey <sergey.qa.63@protonmail.com>
2024-01-07 06:50:51 +0400

    Обновлен файл тестов главного меню. Добавлены подпроверки загрузки страниц после кликов

--- tests/test_02_menu_all-pages.py
+++ tests/test_02_menu_all-pages.py
@@ -2,11 +2,9 @@ from selene import browser, have, be, query
 import requests
 import time
 
-response = requests.get('http://localhost/theveil/theveil/')
-status = response.status_code
-link = 'C:/_test_screenshots/The_Veil_site_func_tests/test_02_menu_all-pages/'
-
 def test_start():
+    response = requests.get('http://localhost/theveil/theveil/')
+    status = response.status_code
     browser.open("/theveil/")
     print('')
     print('Главное меню:Загрузка страницы')
@@ -24,6 +22,8 @@ def test_active_button():
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[2].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[3].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[4].should(have.no.css_class("active"))
+
+link = 'C:/_test_screenshots/The_Veil_site_func_tests/test_02_menu_all-pages/'
 
 def test_hover_buttons_index():
     print('')
@@ -49,6 +49,14 @@ def test_click_on_menu_ind():
     print('')
     print('Главное меню:"index"->"index"')
     browser.element('[id="ind0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/index.html'))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[0].should(have.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[1].should(have.no.css_class("active"))
@@ -60,6 +68,14 @@ def test_click_on_menu_plb():
     print('')
     print('Главное меню:"index"->"playbooks"')
     browser.element('[id="plb0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/playbooks.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/playbooks.html'))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[0].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[1].should(have.css_class("active"))
@@ -87,12 +103,28 @@ def test_hover_buttons_playbooks():
     time.sleep(1.5)
     browser.get(query.screenshot_saved(f'{link}{link2}5.png'))
     browser.element('[id="ind0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/index.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/index.html'))
 
 def test_click_on_menu_mvs():
     print('')
     print('Главное меню:"index"->"moves"')
     browser.element('[id="mvs0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/moves.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/moves.html'))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[0].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[1].should(have.no.css_class("active"))
@@ -120,6 +152,14 @@ def test_hover_buttons_moves():
     time.sleep(1.5)
     browser.get(query.screenshot_saved(f'{link}{link3}5.png'))
     browser.element('[id="ind0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/index.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/index.html'))
 
 
@@ -127,6 +167,14 @@ def test_click_on_menu_tbl():
     print('')
     print('Главное меню:"index"->"tables_custom"')
     browser.element('[id="tbl0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/tables_custom.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/tables_custom.html'))
     browser.all('[class="mainmenu mobile-menu"][name=menu0]>ul>li')[0].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"][name=menu0]>ul>li')[1].should(have.no.css_class("active"))
@@ -159,12 +207,28 @@ def test_hover_buttons_tables_custom():
     time.sleep(1.5)
     browser.get(query.screenshot_saved(f'{link}{link4}5.png'))
     browser.element('[id="ind0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/index.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/index.html'))
 
 def test_click_on_menu_glr():
     print('')
     print('Главное меню:"index"->"gallery"')
     browser.element('[id="glr0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/gallery.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/gallery.html'))
     browser.all('[class="mainmenu mobile-menu"][name=menu0]>ul>li')[0].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"][name=menu0]>ul>li')[1].should(have.no.css_class("active"))
@@ -197,6 +261,14 @@ def test_hover_buttons_gallery():
     time.sleep(1.5)
     browser.get(query.screenshot_saved(f'{link}{link5}5.png'))
     browser.element('[id="ind0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/index.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/index.html'))
 
 def test_cross_click_on_menu():
@@ -204,6 +276,14 @@ def test_cross_click_on_menu():
     print('Главное меню:Кросс-переходы')
     browser.open('/theveil/playbooks.html')
     browser.element('[id="plb0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/playbooks.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/playbooks.html'))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[0].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[1].should(have.css_class("active"))
@@ -211,12 +291,44 @@ def test_cross_click_on_menu():
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[3].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"]>ul>li')[4].should(have.no.css_class("active"))
     browser.element('[id="mvs0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/moves.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/moves.html'))
     browser.element('[id="mvs0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/moves.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/moves.html'))
     browser.element('[id="tbl0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/tables_custom.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/tables_custom.html'))
     browser.element('[id="tbl0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/tables_custom.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/tables_custom.html'))
     browser.all('[class="mainmenu mobile-menu"][name=menu0]>ul>li')[0].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"][name=menu0]>ul>li')[1].should(have.no.css_class("active"))
@@ -228,8 +340,24 @@ def test_cross_click_on_menu():
     browser.all('[class="mainmenu mobile-menu"][name=menu1]>ul>li')[2].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"][name=menu1]>ul>li')[3].should(have.no.css_class("active"))
     browser.element('[id="glr0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/gallery.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/gallery.html'))
     browser.element('[id="glr0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/gallery.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/gallery.html'))
     browser.all('[class="mainmenu mobile-menu"][name=menu0]>ul>li')[0].should(have.no.css_class("active"))
     browser.all('[class="mainmenu mobile-menu"][name=menu0]>ul>li')[1].should(have.no.css_class("active"))
@@ -241,21 +369,93 @@ def test_cross_click_on_menu():
     browser.all('[class ="gallery-controls"]>ul>li')[2].should(have.no.css_class("active"))
     browser.all('[class ="gallery-controls"]>ul>li')[3].should(have.no.css_class("active"))
     browser.element('[id="mvs0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/moves.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/moves.html'))
     browser.element('[id="plb0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/playbooks.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/playbooks.html'))
     browser.element('[id="glr0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/gallery.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/gallery.html'))
     browser.element('[id="tbl0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/tables_custom.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/tables_custom.html'))
     browser.element('[id="mvs0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/moves.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/moves.html'))
     browser.element('[id="glr0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/gallery.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/gallery.html'))
     browser.element('[id="plb0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/playbooks.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/playbooks.html'))
     browser.element('[id="tbl0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/tables_custom.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/tables_custom.html'))
     browser.element('[id="plb0"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/playbooks.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/playbooks.html'))
 
