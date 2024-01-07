f20a2f57f13292e477a55117adad08ad777d00c4
OQASergey <sergey.qa.63@protonmail.com>
2024-01-07 06:51:08 +0400

    Обновлен файл тестов "index". Добавлены подпроверки загрузки страниц после кликов

--- tests/test_03_page_index.py
+++ tests/test_03_page_index.py
@@ -3,22 +3,12 @@ import requests
 from selene.core.wait import Command
 import time
 
-response = requests.get('http://localhost/theveil/theveil/')
-status = response.status_code
-link = 'C:/_test_screenshots/The_Veil_site_func_tests/test_03_page_index/'
-
-def scroll(x: int, y: int) -> Command:
-    return Command(
-        f'scroll page by x {x} y {y}',
-        lambda browser: browser.driver.execute_script(
-            f'window.scrollBy({x}, {y});'
-        )
-    )
-
 def test_start():
     browser.open("/theveil/")
     print('')
     print('"index":Загрузка страницы')
+    response = requests.get('http://localhost/theveil/theveil/')
+    status = response.status_code
     if status is 200:
         print('Статус код: ', status, ' OK')
     else:
@@ -29,17 +19,91 @@ def test_click_on_sectors():
     print('')
     print('"index":Переходы через кнопки разделов')
     browser.element('[class="primary-btn class-btn"][id="plb1"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/playbooks.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/playbooks.html'))
     browser.open('/theveil/index.html')
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/index.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.element('[class="primary-btn class-btn"][id="mvs1"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/moves.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/moves.html'))
     browser.open('/theveil/')
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/index.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.element('[class="primary-btn class-btn"][id="tbl1"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/tables_custom.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/tables_custom.html'))
     browser.open('/theveil/index.html')
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/index.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.element('[class="primary-btn class-btn"][id="glr1"]').click()
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/gallery.html').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
     browser.should(have.url_containing('/theveil/gallery.html'))
     browser.open('/theveil/')
+    #Проверка загрузки
+    status = requests.get('http://localhost/theveil/theveil/').status_code
+    if status is 200:
+        pass
+    else:
+        print('Код: ', status)
+        browser.element('[id="reload-button"]').click()
+        #Конец проверки загрузки
+
+def scroll(x: int, y: int) -> Command:
+    return Command(
+        f'scroll page by x {x} y {y}',
+        lambda browser: browser.driver.execute_script(
+            f'window.scrollBy({x}, {y});'
+        )
+    )
+
+link = 'C:/_test_screenshots/The_Veil_site_func_tests/test_03_page_index/'
 
 def test_scroll_down_up():
     print('')
