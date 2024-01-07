927a3250cf3ebc59d7f8b885fdc44845e26983ad
OQASergey <sergey.qa.63@protonmail.com>
2024-01-07 06:50:29 +0400

    Обновлен файл тестов приветственной страницы. Добавлены подпроверки загрузки страниц после кликов

--- tests/test_01_page_attention.py
+++ tests/test_01_page_attention.py
@@ -3,26 +3,16 @@ import requests
 from selene.core.wait import Command
 import time
 
-response1 = requests.get('http://localhost/theveil/')
-status1 = response1.status_code
-link = 'C:/_test_screenshots/The_Veil_site_func_tests/test_01_page_attention/'
-
-def scroll(x: int, y: int) -> Command:
-    return Command(
-        f'scroll page by x {x} y {y}',
-        lambda browser: browser.driver.execute_script(
-            f'window.scrollBy({x}, {y});'
-        )
-    )
-
 def test_start():
     browser.open("/")
     print('')
     print('Приветственная страница:Загрузка страницы')
-    if status1 is 200:
-        print('Статус код: ', status1, ' OK')
+    response = requests.get('http://localhost/theveil/')
+    status = response.status_code
+    if status is 200:
+        print('Статус код: ', status, ' OK')
     else:
-        print('Код: ', status1)
+        print('Код: ', status)
         browser.element('[id="reload-button"]').click()
 def test_attention_title():
     print('')
@@ -33,34 +23,41 @@ def test_attention_text():
     print('Приветственная страница:Наличие текста предупреждения')
     browser.element('[id="at2"]').should(have.text("Данный сайт является небольшим справочным ресурсом для личного использования по настольной ролевой игре the Veil (авторы: Фрейзер и Кайл Симмонс). Сайт ничего не пропогандирует и не распространяет. Публиковать ссылку на текущий домен, поддомены и страницы на домене или поддомене в любом другом ресурсе строго воспрещается."))
 
+def scroll(x: int, y: int) -> Command:
+    return Command(
+        f'scroll page by x {x} y {y}',
+        lambda browser: browser.driver.execute_script(
+            f'window.scrollBy({x}, {y});'
+        )
+    )
+
+link = 'C:/_test_screenshots/The_Veil_site_func_tests/test_01_page_attention/'
+
 def test_scroll_down_up():
     print('')
-    print('Скролл вниз на 10 000 px')
+    print('Скроллирование')
     browser.perform(scroll(0, 10000))
     time.sleep(1)
     browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_1_down.png'))
-    print('Скролл вверх на 20 000 px')
     browser.perform(scroll(0, -20000))
     time.sleep(1)
     browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_2_up.png'))
-    print('Скролл вправа на 1 000 px')
     browser.perform(scroll(1000, 0))
     time.sleep(1)
     browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_3_right.png'))
-    print('Скролл влево на 2 000 px')
     browser.perform(scroll(-2000, 0))
     time.sleep(1)
     browser.get(query.screenshot_saved(f'{link}test_scroll_down_up_4_left.png'))
 
-response2 = requests.get('http://localhost/theveil/theveil/')
-status2 = response2.status_code
 def test_open_site():
     print('')
     print('Приветственная страница:Переход основной сайт')
+    response = requests.get('http://localhost/theveil/theveil/')
+    status = response.status_code
     browser.element('[class="opbut"]').click()
-    if status2 is 200:
-        print('Статус код: ', status2, ' OK')
+    if status is 200:
+        print('Статус код: ', status, ' OK')
     else:
-        print('Код: ', status2)
+        print('Код: ', status)
         browser.element('[id="reload-button"]').click()
     browser.should(have.url_containing('/theveil/'))
