9c89d04224124357c5617032882e3970e7fae912
OQASergey <sergey.qa.63@protonmail.com>
2024-01-14 02:32:13 +0400

    Обновлен файл тестов "gallery". Измненено время простоя системы для проверки видео с 1 до 2 секунд

--- tests/test_08_page_gallery.py
+++ tests/test_08_page_gallery.py
@@ -560,7 +560,7 @@ def test_video():
     browser.element('[class="mfp-iframe"]').should(
         have.attribute('src','http://www.youtube.com/embed/NjlGjkAFD2w?autoplay=1')
     )
-    time.sleep(1)
+    time.sleep(2)
     browser.get(query.screenshot_saved(f'{link}10_test_video.png'))
     browser.element('[title="Close (Esc)"]').click()
     browser.element('[class="mfp-iframe"]').should(be.absent)
